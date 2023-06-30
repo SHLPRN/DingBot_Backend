ed7f94c3594e0e2328e04ce44d9134311152b659
SHLPRN <20373672@buaa.edu.cn>
2023-06-30 23:39:53 +0800

    feat: 新增管理员登录及管理员添加产品接口

--- DingBot/administrator/urls.py
+++ DingBot/administrator/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = {
-}
+urlpatterns = [
+    path('login/', login, name='login'),
+    path('addProduct/', add_product, name='addProduct'),
+]
--- DingBot/administrator/views.py
+++ DingBot/administrator/views.py
@@ -1,1 +1,90 @@
-from django.shortcuts import render
+import time
+import os.path
+
+from django.http import JsonResponse, FileResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import *
+from utils.token import *
+
+
+PRODUCT_URL = '/media/product/'
+VIEW_URL = '/media/view/'
+CHOICE_URL = '/media/choice/'
+
+
+@csrf_exempt
+def login(request):
+    phone = request.POST.get('phone')
+    password = request.POST.get('password')
+    administrators = Administrator.objects.filter(phone=phone)
+    if not administrators.exists():
+        return JsonResponse({'errno': 1004, 'msg': '用户不存在'})
+    administrator = administrators.first()
+    token = create_token('admin', administrator.id)
+    return JsonResponse({'errno': 0, 'token': token})
+
+
+@csrf_exempt
+def add_product(request):
+    product = Product()
+    product.name = request.POST.get('name')
+    product.description = request.POST.get('description')
+    product.price = float(request.POST.get('price'))
+    product_image = request.FILES.get('image')
+    product.image = "WAIT"
+    product.save()
+    product.image = handle_image(product_image, os.path.join(PRODUCT_URL, f'product_{product.id}' +
+                                                             os.path.splitext(str(product_image.name))[1]))
+    product.save()
+    for category in request.POST.get('category').split(','):
+        product_category = ProductCategory()
+        product_category.product = product
+        product_category.category = Category.objects.get(id=category)
+        product_category.save()
+    view_num = int(request.POST.get('view_num'))
+    module_num = int(request.POST.get('module_num'))
+    view_list = []
+    for order in range(1, view_num + 1):
+        view = View()
+        view.name = request.POST.get(f'view_{order}_name')
+        view_image = request.FILES.get(f'view_{order}_image')
+        view.image = "WAIT"
+        view.product = product
+        view.save()
+        view.image = handle_image(view_image, os.path.join(VIEW_URL, f'view_{view.id}' +
+                                                           os.path.splitext(str(view_image.name))[1]))
+        view.save()
+        view_list.append(view)
+    for order1 in range(1, module_num + 1):
+        module = Module()
+        module.name = request.POST.get(f'module_{order1}_name')
+        module.order = order1
+        module.product = product
+        module.save()
+        choice_num = int(request.POST.get(f'module_{order1}_choice_num'))
+        for order2 in range(1, choice_num + 1):
+            choice = Choice()
+            choice.name = request.POST.get(f'module_{order1}_choice_{order2}_name')
+            choice.price = float(request.POST.get(f'module_{order1}_choice_{order2}_price'))
+            choice.order = order2
+            choice.module = module
+            choice.save()
+            for order3 in range(1, view_num + 1):
+                choice_image = ChoiceImage()
+                choice_image_image = request.FILES.get(f'module_{order1}_choice_{order2}_view_{order3}_image')
+                choice_image.image = "WAIT"
+                choice_image.choice = choice
+                choice_image.view = view_list[order3 - 1]
+                choice_image.save()
+                choice_image.image = handle_image(choice_image_image,
+                                                  os.path.join(CHOICE_URL, f'choice_image_{choice_image.id}' +
+                                                               os.path.splitext(str(choice_image_image.name))[1]))
+                choice_image.save()
+    return JsonResponse({'errno': 0, 'msg': '新建产品成功'})
+
+
+def handle_image(image, path):
+    with open('.' + path, 'wb+') as f:
+        for chunk in image.chunks():
+            f.write(chunk)
+    return path
