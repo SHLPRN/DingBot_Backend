33cd1b70e77793f74326af11bf96dc91e0de3792
SHLPRN <20373672@buaa.edu.cn>
2023-06-30 09:17:47 +0800

    feat: 初始化models

--- DingBot/DingBot/urls.py
+++ DingBot/DingBot/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/admin/', include('admin.urls')),
+    path('api/administrator/', include('administrator.urls')),
+    path('api/customer/', include('customer.urls')),
+    path('api/product/', include('product.urls')),
 ]
