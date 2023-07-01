a865c4eba82c8154d1a42c0ea6de2bd4b5a28b64
SHLPRN <20373672@buaa.edu.cn>
2023-07-01 09:21:58 +0800

    fix: 修复身份认证中间件

--- DingBot/DingBot/middleware.py
+++ DingBot/DingBot/middleware.py
@@ -16,7 +16,7 @@ class AuthorizeMiddleware(MiddlewareMixin):
     def process_request(request):
         if request.path not in API_WHITELIST:
             token = request.META.get('HTTP_TOKEN')
-            if token is None:
+            if token is None or token == "":
                 return JsonResponse({'errno': 1001, 'msg': "未查询到登录信息"})
             else:
                 identify = "user"
