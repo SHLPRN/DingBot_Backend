7b35aaa68678f6e97f806af4dbf509378b234c69
SHLPRN <20373672@buaa.edu.cn>
2023-07-04 14:50:35 +0800

    feat: 新增检查token接口。

--- DingBot/DingBot/middleware.py
+++ DingBot/DingBot/middleware.py
@@ -11,7 +11,7 @@ API_WHITELIST = ["/api/administrator/login/", "/api/product/getProductList/", "/
                  "/api/product/getChoiceImage/", "/api/customer/login/", ]
 ADMIN = ["/api/administrator/addProduct/", "/api/administrator/addModule/", "/api/administrator/addChoiceImage/",
          "/api/administrator/getProductList/", "/api/administrator/getProduct/",
-         "/api/administrator/deleteChoiceImage/", ]
+         "/api/administrator/deleteChoiceImage/", "/api/adminCheckToken/", ]
 
 
 class AuthorizeMiddleware(MiddlewareMixin):
@@ -27,7 +27,10 @@ class AuthorizeMiddleware(MiddlewareMixin):
                     identify = 'admin'
                 status = check_token(identify, token)
                 if status == 0:
-                    pass
+                    if request.path == '/api/adminCheckToken/' or request.path == '/api/checkToken/':
+                        return JsonResponse({'errno': 0, 'msg': '成功'})
+                    else:
+                        pass
                 elif status == 1:
                     return JsonResponse({'errno': 1002, 'msg': "登录已过期"})
                 else:
